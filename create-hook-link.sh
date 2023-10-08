3be6e10703a5edba00df5cc11b928b11a3e5536b
YJ-ArchLinux-Laptop <yj1516268@outlook.com>
2023-10-08 14:36:04 +0800

    +: create-hook-link.sh、hooks —— 添加创建git钩子的脚本和钩子

---
+++ create-hook-link.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+: << !
+Name: create-hook-link.sh
+Author: YJ
+Email: yj1516268@outlook.com
+Created Time: 2023-10-08 14:14:21
+
+Description: 创建git钩子
+
+Attentions:
+-
+
+Depends:
+-
+!
+
+ln -sf ../../hooks/post-commit .git/hooks/post-commit
