2ddd097ce75955e9e77b269831d08b20db0829c8
YJ-ArchLinux-Laptop <yj1516268@outlook.com>
2023-10-13 15:04:27 +0800

    E: create-hook-link.sh —— 更新为遍历钩子文件夹并依次创建

--- create-hook-link.sh
+++ create-hook-link.sh
@@ -4,7 +4,7 @@
 Name: create-hook-link.sh
 Author: YJ
 Email: yj1516268@outlook.com
-Created Time: 2023-10-08 14:14:21
+Created Time: 2023-09-18 13:37:04
 
 Description: 创建git钩子
 
@@ -15,5 +15,11 @@ Depends:
 -
 !
 
-mkdir -p .git/hooks
-ln -sf ../../hooks/post-commit .git/hooks/post-commit
+hook_work_dir=".git/hooks"
+mkdir -p "$hook_work_dir"
+
+hook_base_dir="hooks"
+path=$(ls "$hook_base_dir")
+for file in $path; do
+  ln -sf ../../$hook_base_dir/"$file" "$hook_work_dir/$file"
+done
