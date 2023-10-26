29b6b6780740e5fb5174ddb25058f4df6b727e1e
YJ-ArchLinux-Laptop <yj1516268@outlook.com>
2023-10-26 17:26:08 +0800

    Terminal版本输出信息和备注完善

--- function/httpserver.go
+++ function/httpserver.go
@@ -17,15 +17,16 @@ import (
 
 // 启动HTTP服务（Terminal使用）
 func HttpServer(address string, port string, dir string) {
+	// 创建TCP监听器
 	listener, err := net.Listen("tcp", address+":"+port)
 	if err != nil {
 		fmt.Printf("\x1b[31;1m%s\x1b[0m\n", err)
 	} else {
-		// 成功后输出各种信息
+		// 成功后输出服务信息
 		url := fmt.Sprintf("http://%s:%v", address, port)
-		fmt.Printf("\n\x1b[32;1mStarting http server at %s\x1b[0m\n", dir)                   // 服务地址
-		fmt.Printf("\x1b[32;1mHTTP url is %s\x1b[0m\n", url) // URL
-		codeString, err := QrCodeString(url)                                                 // 二维码
+		fmt.Printf("\n\x1b[32;1mStarting http server at %s\x1b[0m\n", dir) // 服务地址
+		fmt.Printf("\x1b[32;1mHTTP url is %s\x1b[0m\n", url)               // URL
+		codeString, err := QrCodeString(url)                               // 二维码
 		if err != nil {
 			fmt.Printf("\x1b[31;1m%s\x1b[0m\n", err)
 		} else {
@@ -33,7 +34,10 @@ func HttpServer(address string, port string, dir string) {
 		}
 		fmt.Printf("\n\x1b[33;1m%s\x1b[0m\n", "Press Ctrl+C to stop.") // 服务停止快捷键
 
-		if err := http.Serve(listener, http.FileServer(http.Dir(dir))); err != nil {
+		// 创建请求处理器
+		handler := http.FileServer(http.Dir(dir))
+		// 启动服务器
+		if err := http.Serve(listener, handler); err != nil {
 			fmt.Printf("\x1b[31;1m%s\x1b[0m\n", err)
 		}
 	}
