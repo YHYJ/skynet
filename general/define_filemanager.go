c81d2520fdb268fd553daa49ad48aa8379edab57
YJ-ArchLinux-Laptop <yj1516268@outlook.com>
2023-11-15 10:29:05 +0800

    统一相同部分

--- general/define_filemanager.go
+++ general/define_filemanager.go
@@ -10,9 +10,83 @@ Description: 文件管理
 package general
 
 import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
 	"os"
 	"path/filepath"
+	"strings"
+	"sync"
 )
+
+// ReadFileLine 读取文件指定行
+func ReadFileLine(file string, line int) string {
+	// 打开文件
+	text, err := os.Open(file)
+	// 相当于Python的with语句
+	defer text.Close()
+	// 处理错误
+	if err != nil {
+		log.Println(err)
+	}
+	// 行计数
+	count := 1
+	// 创建一个扫描器对象按行遍历
+	scanner := bufio.NewScanner(text)
+	// 逐行读取，输出指定行
+	for scanner.Scan() {
+		if line == count {
+			return scanner.Text()
+		}
+		count++
+	}
+	return ""
+}
+
+// ReadFileKey 读取文件包含指定字符串的行
+func ReadFileKey(file, key string) string {
+	// 打开文件
+	text, err := os.Open(file)
+	// 相当于Python的with语句
+	defer text.Close()
+	// 处理错误
+	if err != nil {
+		log.Println(err)
+	}
+	// 创建一个扫描器对象按行遍历
+	scanner := bufio.NewScanner(text)
+	// 逐行读取，输出指定行
+	for scanner.Scan() {
+		if strings.Contains(scanner.Text(), key) {
+			return scanner.Text()
+		}
+	}
+	return ""
+}
+
+// ReadFileCount 获取文件包含指定字符串的行的计数
+func ReadFileCount(file, key string) int {
+	// 打开文件
+	text, err := os.Open(file)
+	// 相当于Python的with语句
+	defer text.Close()
+	// 处理错误
+	if err != nil {
+		log.Println(err)
+	}
+	// 计数器
+	count := 0
+	// 创建一个扫描器对象按行遍历
+	scanner := bufio.NewScanner(text)
+	// 逐行读取，输出指定行
+	for scanner.Scan() {
+		if strings.Contains(scanner.Text(), key) {
+			count++
+		}
+	}
+	return count
+}
 
 // FileExist 判断文件是否存在
 func FileExist(filePath string) bool {
@@ -31,4 +105,218 @@ func GetAbsPath(filePath string) string {
 	} else {
 		return absPath
 	}
+}
+
+// FileEmpty 判断文件是否为空（无法判断文件夹）
+func FileEmpty(filePath string) bool {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return true
+	}
+	defer file.Close()
+	fi, err := file.Stat()
+	if err != nil {
+		return true
+	}
+	return fi.Size() == 0
+}
+
+// FolderEmpty 判断文件夹是否为空，包括隐藏文件
+func FolderEmpty(filePath string) bool {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return true
+	}
+	defer file.Close()
+
+	_, err = file.Readdir(1)
+	if err == io.EOF {
+		return true
+	}
+	return false
+}
+
+// CreateFile 创建文件，如果其父目录不存在则创建父目录
+func CreateFile(filePath string) error {
+	if FileExist(filePath) {
+		return nil
+	}
+	// 截取filePath的父目录
+	parentPath := filePath[:strings.LastIndex(filePath, "/")]
+	if err := os.MkdirAll(parentPath, os.ModePerm); err != nil {
+		return err
+	}
+	_, err := os.Create(filePath)
+	return err
+}
+
+// CreateDir 创建文件夹，如果其父目录不存在则创建父目录
+func CreateDir(dirPath string) error {
+	if FileExist(dirPath) {
+		return nil
+	}
+	// 截取dirPath的父目录
+	parentPath := dirPath[:strings.LastIndex(dirPath, "/")]
+	if err := os.MkdirAll(parentPath, os.ModePerm); err != nil {
+		return err
+	}
+	return os.Mkdir(dirPath, os.ModePerm)
+}
+
+// GoToDir 进到指定目录
+func GoToDir(dirPath string) error {
+	return os.Chdir(dirPath)
+}
+
+// WriteFile 写入内容到文件
+func WriteFile(filePath string, content string) error {
+	// 文件存在
+	if FileExist(filePath) {
+		// 文件内容为空
+		if FileEmpty(filePath) {
+			// 打开文件并写入内容
+			file, err := os.OpenFile(filePath, os.O_WRONLY|os.O_TRUNC, 0666)
+			if err != nil {
+				return err
+			} else {
+				_, err := file.WriteString(content)
+				if err != nil {
+					return err
+				}
+			}
+		} else {
+			// 文件内容不为空
+			return fmt.Errorf("File %s is not empty", filePath)
+		}
+	} else {
+		// 文件不存在
+		// 创建文件
+		if err := CreateFile(filePath); err != nil {
+			return err
+		}
+		// 打开文件并写入内容
+		file, err := os.OpenFile(filePath, os.O_WRONLY|os.O_TRUNC, 0666)
+		if err != nil {
+			return err
+		} else {
+			_, err := file.WriteString(content)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+// DeleteFile 删除文件
+func DeleteFile(filePath string) error {
+	if !FileExist(filePath) {
+		return nil
+	}
+	return os.Remove(filePath)
+}
+
+// CompareFile 并发比较两个文件是否相同
+func CompareFile(file1Path string, file2Path string) (bool, error) {
+	// 尝试打开文件
+	file1, err := os.Open(file1Path)
+	if err != nil {
+		return false, err
+	}
+	defer file1.Close()
+	file2, err := os.Open(file2Path)
+	if err != nil {
+		return false, err
+	}
+	defer file2.Close()
+
+	// 获取文件大小
+	file1Info, err := file1.Stat()
+	if err != nil {
+		return false, err
+	}
+	file2Info, err := file2.Stat()
+	if err != nil {
+		return false, err
+	}
+	file1Size := file1Info.Size()
+	file2Size := file2Info.Size()
+
+	// 如果文件大小不同，则直接返回不同
+	if file1Size != file2Size {
+		return false, nil
+	}
+
+	// 文件大小相同则（按块）比较文件内容
+	const chunkSize = 1024 * 1024                             // 每次比较的块大小（1MB）
+	numChunks := int((file1Size + chunkSize - 1) / chunkSize) // 计算文件需要被分成多少块
+
+	equal := true                // 文件是否相同的标志位
+	var wg sync.WaitGroup        // wg用于等待所有的goroutine执行完毕，然后关闭errCh通道
+	errCh := make(chan error, 1) // errCh用于接收goroutine执行过程中返回的错误
+
+	for i := 0; i < numChunks; i++ { // 同时比较多个块
+		wg.Add(1)
+		go func(chunkIndex int) {
+			defer wg.Done()
+
+			// 计算当前块的偏移量和大小
+			offset := int64(chunkIndex) * chunkSize
+			size := chunkSize
+			if offset+int64(size) > file1Size {
+				size = int(file1Size - offset)
+			}
+
+			// 创建两个大小为size的buffer
+			buffer1 := make([]byte, size)
+			buffer2 := make([]byte, size)
+
+			// 从文件中读取指定大小的内容到buffer
+			_, err := file1.ReadAt(buffer1, offset)
+			if err != nil && err != io.EOF {
+				errCh <- err
+				return
+			}
+
+			// 从文件中读取指定大小的内容到buffer
+			_, err = file2.ReadAt(buffer2, offset)
+			if err != nil && err != io.EOF {
+				errCh <- err
+				return
+			}
+
+			// 比较两个buffer是否相同
+			if !bytesEqual(buffer1, buffer2) {
+				equal = false
+			}
+		}(i)
+	}
+
+	go func() {
+		wg.Wait()
+		close(errCh)
+	}()
+
+	for err := range errCh {
+		if err != nil {
+			return false, err
+		}
+	}
+
+	return equal, nil
+}
+
+// bytesEqual 比较两个文件的内容是否相同
+func bytesEqual(b1 []byte, b2 []byte) bool {
+	if len(b1) != len(b2) {
+		return false
+	}
+
+	for i := 0; i < len(b1); i++ {
+		if b1[i] != b2[i] {
+			return false
+		}
+	}
+
+	return true
 }
--- general/define_variable.go
+++ general/define_variable.go
@@ -4,7 +4,7 @@ Author: YJ
 Email: yj1516268@outlook.com
 Created Time: 2023-06-08 16:01:45
 
-Description: 操作变量
+Description: 操作变量（包括代码变量和环境变量）
 */
 
 package general
@@ -16,8 +16,80 @@ import (
 	"strconv"
 )
 
+// ---------- 代码变量
+
+// 输出格式变量
+var (
+	// 常规输出格式 1部分: <输出内容>
+	Regelar1PFormat = "%s\n"
+	// 常规输出格式 2部分: <输出内容1><输出内容2>
+	Regelar2PFormat = "%s%s\n"
+
+	// 标题部分输出格式 H1级别标题: <标题文字>
+	TitleH1Format = "\n\x1b[36;3m%s\x1b[0m\n\n"
+
+	// 分隔线输出格式 隐性分隔线: <分隔线>
+	LineHiddenFormat = "\x1b[30m%s\x1b[0m\n"
+	// 分隔线输出格式 显性分隔线: <分隔线>
+	LineShownFormat = "\x1b[30;1m%s\x1b[0m\n"
+
+	// Slice输出格式 切片遍历·2部分: <切片内容1><分隔符><切片内容2>
+	SliceTraverse2PFormat = "\x1b[32;1m%s\x1b[0m%s\x1b[34m%s\x1b[0m\n"
+	// Slice输出格式 带后缀的切片遍历·2部分: <切片内容1><分隔符><切片内容2><分隔符><后缀>
+	SliceTraverse2PSuffixFormat = "\x1b[32;1m%s\x1b[0m%s\x1b[34m%s\x1b[0m%s%s\n"
+	// Slice输出格式 带后缀的切片遍历·2部分·不换行: <切片内容1><分隔符><切片内容2><分隔符><后缀>
+	SliceTraverse2PSuffixNoNewLineFormat = "\x1b[32;1m%s\x1b[0m%s\x1b[34m%s\x1b[0m%s%s"
+	// Slice输出格式 带后缀的切片遍历·3部分: <切片内容1><分隔符><切片内容2><分隔符><切片内容3><分隔符><后缀>
+	SliceTraverse3PSuffixFormat = "\x1b[32;1m%s\x1b[0m%s\x1b[34m%s\x1b[0m%s\x1b[33;1m%s\x1b[0m%s%s\n"
+	// Slice输出格式 带后缀的切片遍历·4部分: <切片内容1><分隔符><切片内容2><分隔符><切片内容3><分隔符><切片内容4><分隔符><后缀>
+	SliceTraverse4PSuffixFormat = "\x1b[32;1m%s\x1b[0m%s\x1b[34m%s\x1b[0m%s\x1b[33m%s\x1b[0m%s\x1b[35;1m%s\x1b[0m%s%s\n"
+
+	// 成功信息输出格式 成功信息: <成功信息>
+	SuccessFormat = "\x1b[32;1m%s\x1b[0m\n"
+	// 成功信息输出格式 成功信息·不换行: <成功信息>
+	SuccessNoNewLineFormat = "\x1b[32;1m%s\x1b[0m"
+	// 成功信息输出格式 带后缀的成功信息: <成功信息><分隔符><后缀>
+	SuccessSuffixFormat = "\x1b[32;1m%s\x1b[0m%s%s\n"
+	// 成功信息输出格式 带后缀的成功信息·不换行: <成功信息><分隔符><后缀>
+	SuccessSuffixNoNewLineFormat = "\x1b[32;1m%s\x1b[0m%s%s"
+
+	// 提示信息输出格式 带前缀的提示信息: <提示信息>
+	TipsPrefixFormat = "%s%s\x1b[32;1m%s\x1b[0m\n"
+	// 提示信息输出格式 带后缀的提示信息·2部分·不换行: <提示信息1><分隔符><提示信息2><分隔符><后缀>
+	Tips2PSuffixNoNewLineFormat = "\x1b[32;1m%s\x1b[0m%s\x1b[36;1m%s\x1b[0m%s%s"
+
+	// 展示信息输出格式 展示信息: <展示信息>
+	InfoFormat = "\x1b[33;1m%s\x1b[0m\n"
+	// 展示信息输出格式 带前缀的展示信息: <前缀><分隔符><展示信息>
+	InfoPrefixFormat = "%s%s\x1b[33;1m%s\x1b[0m\n"
+	// 展示信息输出格式 带前缀的展示信息·2部分: <前缀><分隔符><展示信息1><分隔符><展示信息2>
+	Info2PPrefixFormat = "%s%s\x1b[33;1m%s\x1b[0m%s\x1b[35m%s\x1b[0m\n"
+	// 展示信息输出格式 带后缀的展示信息: <展示信息><分隔符><后缀>
+	InfoSuffixFormat = "\x1b[33;1m%s\x1b[0m%s%s\n"
+	// 展示信息输出格式 带前后缀的展示信息: <前缀><分隔符><展示信息><分隔符><后缀>
+	InfoPrefixSuffixFormat = "%s%s\x1b[33;1m%s\x1b[0m%s%s\n"
+
+	// 错误信息输出格式 基础错误: <错误信息>
+	ErrorBaseFormat = "\x1b[31m%s\x1b[0m\n"
+	// 错误信息输出格式 带后缀的错误: <错误信息><分隔符><后缀>
+	ErrorSuffixFormat = "\x1b[31m%s\x1b[0m%s%s\n"
+)
+
+// ---------- 环境变量
+
 // 操作系统
 var Platform = runtime.GOOS
+
+// 用户名，当程序提权运行时，使用SUDO_USER变量获取提权前的用户名
+var UserName = func() string {
+	if GetVariable("SUDO_USER") != "" {
+		return GetVariable("SUDO_USER")
+	}
+	return GetVariable("USER")
+}()
+
+// 用户信息
+var UserInfo, _ = GetUserInfoByName(UserName)
 
 // 用来处理不同系统之间的变量名差异
 var platformChart = map[string]map[string]string{
@@ -65,7 +137,7 @@ func GetUserInfoByName(userName string) (*user.User, error) {
 	return userInfo, nil
 }
 
-// GetUserInfoById 根据ID获取用户信息
+// GetUserInfoById 根据 ID 获取用户信息
 func GetUserInfoById(userId int) (*user.User, error) {
 	userInfo, err := user.LookupId(strconv.Itoa(userId))
 	if err != nil {
