377148b8b3e7fa34e6dfe81d8d5e3a7c930b3099
YJ-ArchLinux-Laptop <yj1516268@outlook.com>
2024-07-05 11:05:22 +0800

    v0.11.5

--- general/version.go
+++ general/version.go
@@ -16,7 +16,7 @@ import (
 
 const (
 	Name    string = "Skynet"                 // 程序名
-	Version string = "v0.11.4"                // 程序版本
+	Version string = "v0.11.5"                // 程序版本
 	Project string = "github.com/yhyj/skynet" // 项目地址
 )
 
